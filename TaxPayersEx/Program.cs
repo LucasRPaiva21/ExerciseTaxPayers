31aa4b4ebbdc37ae8e5f2c104e6db0d75a0b6841
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-15 22:12:43 -0300

    Projeto Inicializado

---
+++ TaxPayersEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaxPayersEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
